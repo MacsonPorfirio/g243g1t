76bb2e2c85146b5c97d4e3327908bf4524d9045e
macporfirio <110197679+MacsonPorfirio@users.noreply.github.com>
2024-05-22 08:44:41 -0300

    Add files via upload

--- mercado.js
+++ mercado.js
@@ -34,17 +34,104 @@ const Cliente = (_id,_itens) =>{
         itens:_itens
       }
 }
+
 const caixaChamaProximo = () =>{
     let cliente = f1.desenfileirar();
     let tempo = 200;
     if(cliente){
-        X
+        document.getElementById("C1").innerHTML = cliente;
+        "["+cliente.id + "("+cliente.itens+")]";
         tempo = cliente.itens * 1000;
-        setTimeout(caixaChamaProximo,tempo);
+        setTimeout(function(){
+            caixaChamaProximo();
+     },tempo);
+}else{
+    if(clientesParaAtender > 0){
+        setTimeout(function(){
+            caixaChamaProximo();
+        },gerarItens()*50);
+
+    }
+  }
+}
+const caixa2ChamaProximo = () =>{
+    let cliente = f1.desenfileirar();
+    let tempo = 200;
+    if(cliente){
+        document.getElementById("C2").innerHTML = cliente;
+        "["+cliente.id + "("+cliente.itens+")]";
+        tempo = cliente.itens * 1000;
+        setTimeout(function(){
+            caixa2ChamaProximo();
+     },tempo);
+}else{
+    if(clientesParaAtender > 0){
+        setTimeout(function(){
+            caixa2ChamaProximo();
+        },gerarItens()*50);
+
     }
 }
+}
+const caixa3ChamaProximo = () =>{
+    let cliente = f1.desenfileirar();
+    let tempo = 200;
+    if(cliente){
+        document.getElementById("C3").innerHTML = cliente;
+        "["+cliente.id + "("+cliente.itens+")]";
+        tempo = cliente.itens * 1000;
+        setTimeout(function(){
+            caixa3ChamaProximo();
+     },tempo);
+}else{
+    if(clientesParaAtender > 0){
+        setTimeout(function(){
+            caixa3ChamaProximo();
+        },gerarItens()*50);
+
+    }
+}
+}
+const caixa4ChamaProximo = () =>{
+    let cliente = f1.desenfileirar();
+    let tempo = 200;
+    if(cliente){
+        document.getElementById("C4").innerHTML = cliente;
+        "["+cliente.id + "("+cliente.itens+")]";
+        tempo = cliente.itens * 1000;
+        setTimeout(function(){
+            caixa4ChamaProximo();
+     },tempo);
+}else{
+    if(clientesParaAtender > 0){
+        setTimeout(function(){
+            caixa4ChamaProximo();
+        },gerarItens()*50);
+
+    }
+}
+}
+const maxClientes = 50;
+const filaInicial = 5;
+let clientesParaAtender = maxClientes - filaInicial;
+let idCliente = filaInicial + 1;
+const entrarNaFila = () =>{
+    if(clientesParaAtender > 0){
+    f1.enfileirar(Cliente("C"+i,gerarItens()));
+    clientesParaAtender--;
+    idCliente++;
+    setTimeout(entrarNaFila,gerarItens()*500);
+    }
+
+}
+
 (function(){
-    for(let i=1; i <= 15; i++){
+    for(let i=1; i <= filaInicial; i++){
         f1.enfileirar(Cliente("C"+i,gerarItens()));
     }
+    caixaChamaProximo();
+    caixa2ChamaProximo();
+    caixa3ChamaProximo();
+    caixa4ChamaProximo();
+    entrarNaFila();
 })();
